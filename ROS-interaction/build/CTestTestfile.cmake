ec823ae4a6ffaebd40ffcadaa3de1f36ae440464
Skylar_xty <skylar.xty@gmail.com>
2023-04-13 19:38:17 +0800

    Series 1

---
+++ ROS-interaction/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/skylar-xty/Add_Pan/ROS1/ROS-interaction/src
+# Build directory: /home/skylar-xty/Add_Pan/ROS1/ROS-interaction/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("class1_ros_qt_demo")
---
+++ ROS-interaction/build/class1_ros_qt_demo/CMakeFiles/ros_qt_demo.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/skylar-xty/Add_Pan/ROS1/ROS-interaction/devel/lib/class1_ros_qt_demo/ros_qt_demo"
+  "/home/skylar-xty/Add_Pan/ROS1/ROS-interaction/devel/lib/class1_ros_qt_demo/ros_qt_demo.pdb"
+  "CMakeFiles/ros_qt_demo.dir/include/class1_ros_qt_demo/moc_main_window.cpp.o"
+  "CMakeFiles/ros_qt_demo.dir/include/class1_ros_qt_demo/moc_qnode.cpp.o"
+  "CMakeFiles/ros_qt_demo.dir/qrc_images.cpp.o"
+  "CMakeFiles/ros_qt_demo.dir/src/main.cpp.o"
+  "CMakeFiles/ros_qt_demo.dir/src/main_window.cpp.o"
+  "CMakeFiles/ros_qt_demo.dir/src/qnode.cpp.o"
+  "include/class1_ros_qt_demo/moc_main_window.cpp"
+  "include/class1_ros_qt_demo/moc_qnode.cpp"
+  "qrc_images.cpp"
+  "ui_main_window.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_qt_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
